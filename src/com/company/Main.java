97618e0447464a7f9497d98966f032d90487a082
Bincharm <aisynai@mail.ru>
2020-11-20 18:45:50 +0600

    test change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Ball ballOne = new Ball();
+        Ball ballThree = new Ball();
 
-        ballOne.anotherColor = "BLUE"; //null
-        ballOne.price = 20;
-        ballOne.radius = 6;
-        ballOne.producerCountry = "GERMANY";
+        ballThree.anotherColor = "BLUE"; //null
+        ballThree.price = 20;
+        ballThree.radius = 6;
+        ballThree.producerCountry = "GERMANY";
 
-        System.out.println(ballOne.anotherColor + " " + ballOne.price + " " + ballOne.radius + " " + ballOne.producerCountry);
+        System.out.println(ballThree.anotherColor + " " + ballThree.price + " " + ballThree.radius + " " + ballThree.producerCountry);
 
 
 
